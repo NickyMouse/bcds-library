f741a1ddda7a20c2932e8cbc77a3c55533618f04
zimmem <zimmem@31de3f4a-a761-11de-be8d-735280eae207>
2009-09-29 07:20:59 +0000

    ''

--- trunk/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/sample/SampleController.java
+++ trunk/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/sample/SampleController.java
@@ -1,6 +1,8 @@
 package com.alibaba.intl.bcds.goldroom.web.sample;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,17 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractCommandController;
+import org.springframework.web.servlet.mvc.SimpleFormController;
 
 import com.alibaba.intl.bcds.goldroom.sample.User;
 import com.alibaba.intl.bcds.goldroom.sample.service.UserService;
 
-public class SampleController extends AbstractCommandController {
-
-    public SampleController() {
-        setCommandClass(User.class);
-        setCommandName("user");
-    }
+public class SampleController extends SimpleFormController {
 
     private UserService userService;
 
@@ -30,14 +27,15 @@ public class SampleController extends AbstractCommandController {
     }
 
     @Override
-    protected ModelAndView handle(HttpServletRequest request, HttpServletResponse response,
-                                  Object command, BindException errors) throws Exception {
+    protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+                                    Object command, BindException errors) throws Exception {
         User user = (User) command;
         if (user != null && StringUtils.isNotEmpty(user.getUsername())) {
             userService.addUser(user);
         }
         List<User> list = userService.listAllUser();
-        return new ModelAndView("sample", "list", list);
+        Map<String, List<User>> map = new HashMap<String, List<User>>();
+        map.put("list", list);
+        return showForm(request, response, errors, map);
     }
-
 }
