f7953a445bfbe5fc94e7cb66981e857484702400
xulinjiang.com <xulinjiang.com@31de3f4a-a761-11de-be8d-735280eae207>
2009-11-03 11:09:26 +0000

    xulinjiang

--- branches/dev1/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/UserInfoModifyController.java
+++ branches/dev1/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/UserInfoModifyController.java
@@ -1,24 +1,41 @@
 package com.alibaba.intl.bcds.goldroom.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
-import com.alibaba.intl.bcds.goldroom.dataobject.Member;
+import com.alibaba.intl.bcds.goldroom.dataobject.User;
 import com.alibaba.intl.bcds.goldroom.service.UserService;
+import com.alibaba.intl.bcds.goldroom.util.MD5;
+import com.alibaba.intl.bcds.goldroom.web.utils.UserUtil;
 
 public class UserInfoModifyController extends SimpleFormController {
 
 	private UserService userService;
 
-	public void setUsersService(UserService usersService) {
-		this.userService = usersService;
+	public void setUserService(UserService userService) {
+		this.userService = userService;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
-    protected ModelAndView onSubmit(Object command) throws Exception {
-		Member member = (Member) command;
-		// 更改用户信息
-		userService.updateUserInfo(member);
-		return new ModelAndView("/index.jsp");
+	protected ModelAndView onSubmit(Object command) throws Exception {
+		User userInfo = (User) command;
+		Map map = new HashMap();
+		if (userInfo.getNewPassword().equals(userInfo.getConfirmPassword())) {
+			// MD5加密
+			map.put("password", MD5
+					.getMD5(userInfo.getNewPassword().getBytes()));
+			// 登录名
+			map.put("login_id", UserUtil.getLoginId());
+			// 更改用户信息
+			userService.updateUserInfo(map);
+			return new ModelAndView(getSuccessView());
+		} else {
+			return new ModelAndView("userInfoModify");
+		}
 	}
+
 }
