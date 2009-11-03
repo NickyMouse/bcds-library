fde65d6c7fbe5aa446286fe7fada724751a327dc
zimmem <zimmem@31de3f4a-a761-11de-be8d-735280eae207>
2009-11-03 13:57:50 +0000

    ''

--- branches/dev1/goldroom-dal/src/test/java/com/alibaba/intl/bcds/goldroom/dao/ibatis/BookInfoDaoTest.java
+++ branches/dev1/goldroom-dal/src/test/java/com/alibaba/intl/bcds/goldroom/dao/ibatis/BookInfoDaoTest.java
@@ -1,53 +1,58 @@
 package com.alibaba.intl.bcds.goldroom.dao.ibatis;
 
 import java.util.Date;
+import java.util.List;
 
-import junit.framework.TestCase;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.intl.bcds.goldroom.dao.BookInfoDao;
+import com.alibaba.intl.bcds.goldroom.dao.test.BaseTest;
 import com.alibaba.intl.bcds.goldroom.dataobject.BookInfo;
 
-public class BookInfoDaoTest extends TestCase{
+@Transactional()
+@TransactionConfiguration(defaultRollback = true)
+public class BookInfoDaoTest extends BaseTest {
 
-	BookInfoDao bookInfoDao;
-	@Override
-	protected void setUp() throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("goldroom-dal.xml");
-		bookInfoDao = (BookInfoDao) context.getBean("bookInfoDao");
-	}
-	
-	@Test
-	public void testDeleteById() {
-		bookInfoDao.deleteById(0);
-	}
+    @Autowired
+    BookInfoDao bookInfoDao;
 
-	@Test
-	public void testInsert() {
-		BookInfo bookInfoDO = new BookInfo();
-		bookInfoDO.setAuthor("Author");
-		bookInfoDO.setCategoryId(1);
-		bookInfoDO.setCategoryName("Tec");
-		bookInfoDO.setPublishTime(new Date());
-		bookInfoDO.setIsbn("1222-5431-2123-2321");
-		bookInfoDao.insert(bookInfoDO);
-	}
-	
-	@Test
-	public void testListAll() {
-		bookInfoDao.listAll();
-	}
+    //@Test
+    public void testDeleteById() {
+        bookInfoDao.deleteById(0);
+    }
 
-	@Test
-	public void testUpdateById() {
-		//fail("Not yet implemented");
-	}
+    @Test
+    public void testInsert() {
+        BookInfo bookInfoDO = new BookInfo();
+        bookInfoDO.setAuthor("中文");
+        bookInfoDO.setCategoryId(1);
+        bookInfoDO.setCategoryName("Tec");
+        bookInfoDO.setPublishTime(new Date());
+        bookInfoDO.setIsbn("1222-5431-2123-2321");
+        bookInfoDao.insert(bookInfoDO);
+    }
 
-	@Test
-	public void testFindById() {
-		//fail("Not yet implemented");
-	}
+    @Test
+    public void testListAll() {
+        List<BookInfo> list = bookInfoDao.listAll();
+        for (BookInfo bookInfo : list) {
+            System.out.println(bookInfo.getAuthor());
+        }
+        //assertEquals(bookInfoDao.listAll().get(3).getAuthor(), "中文");
+    }
+
+    @Test
+    public void testUpdateById() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFindById() {
+        //fail("Not yet implemented");
+    }
 
 }
