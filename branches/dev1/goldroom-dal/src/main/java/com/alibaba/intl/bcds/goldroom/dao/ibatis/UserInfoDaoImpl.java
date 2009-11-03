85c7f6928ddc21f133039b0878131eb01f1b7cad
xulinjiang.com <xulinjiang.com@31de3f4a-a761-11de-be8d-735280eae207>
2009-11-03 10:45:08 +0000

    xulinjiang

--- branches/dev1/goldroom-dal/src/main/java/com/alibaba/intl/bcds/goldroom/dao/ibatis/UserInfoDaoImpl.java
+++ branches/dev1/goldroom-dal/src/main/java/com/alibaba/intl/bcds/goldroom/dao/ibatis/UserInfoDaoImpl.java
@@ -1,16 +1,19 @@
 package com.alibaba.intl.bcds.goldroom.dao.ibatis;
 
+import java.util.Map;
+
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import com.alibaba.intl.bcds.goldroom.dao.UserInfoDao;
-import com.alibaba.intl.bcds.goldroom.dataobject.Member;
 
 public class UserInfoDaoImpl extends SqlMapClientDaoSupport implements
 		UserInfoDao {
 	/**
 	 * 更改用户信息
 	 */
-	public void update(Member member) {
-		getSqlMapClientTemplate().update("", member);
+	@SuppressWarnings("unchecked")
+	public void update(Map userInfo) {
+		getSqlMapClientTemplate().update("MEMBER.updatePassword", userInfo);
 	}
+
 }
