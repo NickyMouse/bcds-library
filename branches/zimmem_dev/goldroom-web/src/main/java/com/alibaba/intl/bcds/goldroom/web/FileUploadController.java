903181b1fe6ad8b7e16d5d7ad331daef3aefc05f
zimmem <zimmem@31de3f4a-a761-11de-be8d-735280eae207>
2010-01-11 12:57:21 +0000

    ''

--- branches/zimmem_dev/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/FileUploadController.java
+++ branches/zimmem_dev/goldroom-web/src/main/java/com/alibaba/intl/bcds/goldroom/web/FileUploadController.java
@@ -1,87 +1,69 @@
 package com.alibaba.intl.bcds.goldroom.web;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.validation.BindException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
+import com.alibaba.intl.bcds.goldroom.util.ImageUtil;
 import com.alibaba.intl.bcds.goldroom.web.command.UploadFileCommand;
 
 public class FileUploadController extends SimpleFormController {
-	private String uploadRoot;
+    private static Logger logger = Logger.getLogger(FileUploadController.class);
+    private ImageUtil     imageUtil;
 
-	public void setUploadRoot(String uploadRoot) {
-		this.uploadRoot = uploadRoot;
-	}
+    protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+                                    Object command, BindException errors) {
 
-	protected ModelAndView onSubmit(HttpServletRequest request,
-			HttpServletResponse response, Object command, BindException errors) {
-		try {
-			UploadFileCommand bean = (UploadFileCommand) command;
-			String isbn = bean.getIsbn();
-			MultipartFile file = bean.getFile();
+        UploadFileCommand bean = (UploadFileCommand) command;
+        String isbn = bean.getIsbn();
+        MultipartFile file = bean.getFile();
 
-			if (file == null) {
-				throw new Exception("");
-			}
-			if (file.getSize() > 10000000) {
-				throw new Exception("");
-			}
+        if (file == null) {
+            return null;
+        }
+        if (file.getSize() > 10000000) {
+            return null;
+        }
 
-			String orginalName = file.getOriginalFilename();
-			String extName = orginalName.substring(
-					orginalName.lastIndexOf("."), orginalName.length());
-			String fileName = isbn + extName;
+        String orginalName = file.getOriginalFilename();
+        String extName = orginalName.substring(orginalName.lastIndexOf("."), orginalName.length());
 
-			if (file.getSize() > 0) {
-				try {
-					saveFileFromInputStream(file.getInputStream(), uploadRoot,
-							fileName);
-				} catch (IOException e) {
-					System.out.println(e.getMessage());
-					return null;
-				}
-			} else {
-				throw new Exception("");
-			}
-			try {
-				// return super.onSubmit(request, response, command, errors);
-				ModelAndView modelAndView = new ModelAndView(
-						"redirect:fillBookInfo.htm");
-				modelAndView.addObject("imgSrc", fileName);
-				modelAndView.addObject("isbn", isbn);
-				return modelAndView;
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				return null;
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return null;
-		}
+        String path = null;
+        try {
+            path = getImageUtil().save(isbn, extName, file.getBytes());
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return null;
+        }
+        try {
+            response.getWriter().write(path);
+            response.setStatus(200);
+            response.setContentType("Text");
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
 
-	}
+    }
 
-	public void saveFileFromInputStream(InputStream stream, String path,
-			String filename) throws IOException {
-		FileOutputStream fs = new FileOutputStream(path + "/" + filename);
-		byte[] buffer = new byte[1024 * 1024];
-		int bytesum = 0;
-		int byteread = 0;
+    /**
+     * @param imageUtil the imageUtil to set
+     */
+    public void setImageUtil(ImageUtil imageUtil) {
+        this.imageUtil = imageUtil;
+    }
 
-		while ((byteread = stream.read(buffer)) != -1) {
-			bytesum += byteread;
-			fs.write(buffer, 0, byteread);
-			fs.flush();
-		}
-		fs.close();
-		stream.close();
-	}
+    /**
+     * @return the imageUtil
+     */
+    public ImageUtil getImageUtil() {
+        return imageUtil;
+    }
 }
