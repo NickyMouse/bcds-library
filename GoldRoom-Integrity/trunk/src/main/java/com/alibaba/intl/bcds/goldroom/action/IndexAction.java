760daf8df8cc2d032bfbcfab8593460dcdb32137
chaosen.lincs@gmail.com <chaosen.lincs@gmail.com@31de3f4a-a761-11de-be8d-735280eae207>
2010-10-20 13:11:01 +0000

    加入excute方法

--- GoldRoom-Integrity/trunk/src/main/java/com/alibaba/intl/bcds/goldroom/action/IndexAction.java
+++ GoldRoom-Integrity/trunk/src/main/java/com/alibaba/intl/bcds/goldroom/action/IndexAction.java
@@ -5,11 +5,9 @@ package com.alibaba.intl.bcds.goldroom.action;
 
 import com.alibaba.intl.bcds.goldroom.action.base.BaseAction;
 
-
-
 /**
  * @author Harrison
- *
+ * 
  */
 public class IndexAction extends BaseAction {
 
@@ -18,5 +16,9 @@ public class IndexAction extends BaseAction {
 	 */
 	private static final long serialVersionUID = -2235532196343484766L;
 
+	public String execute() throws Exception {
+
+		return SUCCESS;
+	}
 
 }
