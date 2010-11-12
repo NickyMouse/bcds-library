7dd35e2ed69cbbe11f3fb7102f5996bd40519f8b
chaosen.lincs@gmail.com <chaosen.lincs@gmail.com@31de3f4a-a761-11de-be8d-735280eae207>
2010-11-12 03:34:25 +0000

    换成hibernate的GPS

---
+++ GoldRoom-Integrity/trunk/src/main/java/com/alibaba/intl/bcds/goldroom/search/commons/CustomizedHibernateGpsDevice.java
@@ -0,0 +1,38 @@
+package com.alibaba.intl.bcds.goldroom.search.commons;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.compass.gps.device.hibernate.HibernateGpsDevice;
+
+/**
+ * the customized JPA GPS Device <br/>
+ * add select query function to the JPA GPS Device <br/>
+ * the select query is the query where the index is built used
+ *
+ * @author Giraffe
+ */
+public class CustomizedHibernateGpsDevice extends HibernateGpsDevice {
+
+    private static Logger       logger = Logger.getLogger(CustomizedHibernateGpsDevice.class);
+    private Map<String, String> entityQueryMap;
+
+    public void initSelectQuery() {
+        if (entityQueryMap != null) {
+            for (String key : entityQueryMap.keySet()) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("selectQuery added: key=" + key + ",  value=" + entityQueryMap.get(key));
+                }
+                this.setIndexSelectQuery(key, entityQueryMap.get(key));
+            }
+        }
+    }
+
+    public void setEntityQueryMap(Map<String, String> entityQueryMap) {
+        this.entityQueryMap = entityQueryMap;
+    }
+
+    public Map<String, String> getEntityQueryMap() {
+        return entityQueryMap;
+    }
+}
