10a52dd03e53a4ee20f6406ea33d639627c95299
xiaogy1012 <xiaogy1012@31de3f4a-a761-11de-be8d-735280eae207>
2011-01-11 05:21:00 +0000

    add proxy to intranet service

--- GoldRoom-Integrity/trunk/src/main/java/com/alibaba/intl/bcds/goldroom/service/IntranetService.java
+++ GoldRoom-Integrity/trunk/src/main/java/com/alibaba/intl/bcds/goldroom/service/IntranetService.java
@@ -3,32 +3,49 @@
  */
 package com.alibaba.intl.bcds.goldroom.service;
 
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
 import org.apache.axis.client.Call;
 import org.apache.axis.client.Service;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.alibaba.intl.bcds.goldroom.dataobject.AlibabaStaffDTO;
 
 /**
  * @author Harrison
- *
+ * 
  */
 public class IntranetService {
-	
-	public static final String INTRANET_USER="aliresume";
-	public static final String INTRANET_PASS="1C3033A82169A15F4825777E0036B10E";
-	
+
+	public static final String INTRANET_USER = "aliresume";
+	public static final String INTRANET_PASS = "1C3033A82169A15F4825777E0036B10E";
+	private String socksProxyHost;
+	private String socksProxyPort;
+
 	private final Logger log = Logger.getLogger(IntranetService.class);
-	
+
+	public void init() {
+		if (StringUtils.isNotBlank(socksProxyHost)
+				&& StringUtils.isNotBlank(socksProxyPort)) {
+			System.setProperty("socksProxyHost", socksProxyHost);
+			System.setProperty("socksProxyPort", socksProxyPort);
+		}
+	}
+
 	/**
 	 * 获取员工信息
+	 * 
 	 * @param strEmail
 	 * @param systemid
 	 * @param password
 	 * @return
 	 */
-	public AlibabaStaffDTO getUserInfoByEmail(String strEmail){
-		
+	public AlibabaStaffDTO getUserInfoByEmail(String strEmail) {
+
 		try {
 			final String endpoint = "http://service.cn.alibaba-inc.com:80/service/webservice.nsf/GetUserVerifyByEmail?WSDL";
 			Service service = new Service();
@@ -37,13 +54,13 @@ public class IntranetService {
 			call.setTargetEndpointAddress(new java.net.URL(endpoint));
 			call.setOperationName("GetUserVerifyByEmail");
 
-			String[] result = (String[]) call
-					.invoke(new Object[] { strEmail,
-							IntranetService.INTRANET_USER,
-							IntranetService.INTRANET_PASS});
-			
-			log.info(" obtained the staff info , name: " + result[1] + " staffId:" + result[0]);
-			
+			String[] result = (String[]) call.invoke(new Object[] { strEmail,
+					IntranetService.INTRANET_USER,
+					IntranetService.INTRANET_PASS });
+
+			log.info(" obtained the staff info , name: " + result[1]
+					+ " staffId:" + result[0]);
+
 			AlibabaStaffDTO aliStaff = new AlibabaStaffDTO();
 			aliStaff.setStaffId(result[0]);
 			aliStaff.setName(result[1]);
@@ -55,11 +72,27 @@ public class IntranetService {
 			aliStaff.setMobile(result[7]);
 			aliStaff.setEmail(result[8]);
 			aliStaff.setAliTalkId(result[9]);
-			
+
 			return aliStaff;
 		} catch (Exception e) {
-			log.error(" get user info error ~!",e);
+			log.error(" get user info error ~!", e);
 		}
-        return null;
-    }
+		return null;
+	}
+
+	public void setSocksProxyHost(String socksProxyHost) {
+		this.socksProxyHost = socksProxyHost;
+	}
+
+	public String getSocksProxyHost() {
+		return socksProxyHost;
+	}
+
+	public void setSocksProxyPort(String socksProxyPort) {
+		this.socksProxyPort = socksProxyPort;
+	}
+
+	public String getSocksProxyPort() {
+		return socksProxyPort;
+	}
 }
